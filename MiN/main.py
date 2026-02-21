e293500b0cd089f8959848bae39729ee3c355249
thuwng <122499901+thuwng@users.noreply.github.com>
2026-02-21 09:36:45 +0700

    Fix

--- MiN/main.py
+++ MiN/main.py
@@ -1,7 +1,25 @@
 import json
 import argparse
 import os
+import subprocess
 from trainer.BaseTrainer import train
+
+
+def download_cub(root_path):
+    url = "https://s3.amazonaws.com/fast-ai-imageclas/CUB_200_2011.tgz"
+    tgz_path = os.path.join(root_path, "CUB_200_2011.tgz")
+    extract_path = os.path.join(root_path, "CUB_200_2011")
+
+    if not os.path.exists(extract_path):
+        print("Downloading CUB-200-2011...")
+        subprocess.run(["wget", url, "-O", tgz_path], check=True)
+
+        print("Extracting...")
+        subprocess.run(["tar", "-xzf", tgz_path, "-C", root_path], check=True)
+
+        print("Done extracting.")
+
+    return extract_path
 
 
 def main():
@@ -12,10 +30,12 @@ def main():
 
     args = {**base_param, **model_param}
 
-    if os.path.exists('/kaggle/input'):
-        args['data_root'] = '/kaggle/input/cub200/CUB_200_2011'
+    if os.path.exists('/kaggle'):
+        working_root = "/kaggle/working"
+        cub_path = download_cub(working_root)
+        args['data_root'] = cub_path
     else:
-        args['data_root'] = './data/CUB_200_2011'
+        args['data_root'] = "./data/CUB_200_2011"
 
     train(args)
 
@@ -28,10 +48,8 @@ def load_json(settings_path):
 
 def setup_parser():
     parser = argparse.ArgumentParser(description='Choose your configs.')
-    parser.add_argument('--base_configs', type=str, required=True,
-                        help='Json file of base settings.')
-    parser.add_argument('--model_configs', type=str, required=True,
-                        help='Json file of model settings.')
+    parser.add_argument('--base_configs', type=str, required=True)
+    parser.add_argument('--model_configs', type=str, required=True)
     return parser
 
 
